c37550c338be68cce67ad7759bdf5ff76a0e7828
Aleksi G. <aleksigoulet@gmail.com>
2025-09-07 19:01:23 -0600

    Move 'send push token' button to dev settings

--- app/(tabs)/settings/developper.tsx
+++ app/(tabs)/settings/developper.tsx
@@ -1,7 +1,53 @@
 import { View, SafeAreaView, Text, Button, StyleSheet } from "react-native"
 import { removeValue } from "@/utils/storageHandlers";
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
 
 export default function Developper() {
+  const handleSendPushCode = async () => {
+    try {
+      const projectId =
+        Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+      if (!projectId) {
+        throw new Error('Project ID not found');
+      }
+
+      const tokenToSend = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+
+      console.log('sending token: ' + tokenToSend);
+
+      const endpoint: string = process.env.EXPO_PUBLIC_NOTIFICATION_SERVER_ADD_TOKEN_URL as string;
+
+      console.log(endpoint);
+
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Api-Key ${process.env.EXPO_PUBLIC_NOTIFICATION_SERVER_API_KEY}`
+        },
+        body: JSON.stringify({
+          token: tokenToSend
+        })
+      })
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+
+    } catch (error) {
+      console.warn('Error submitting push token to server: ' + error)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>For Development. This button deletes all game progress data from AsyncStorage.</Text>
@@ -60,6 +106,10 @@ export default function Developper() {
           }
         }}
       />
+      <Button 
+            title="Send Push Code"
+            onPress={handleSendPushCode}
+          />
     </View>
   )
 }
